4a6001131475ca289f3089ba9c258e05f295410c
iztanpy <61683649+iztanpy@users.noreply.github.com>
2022-10-24 20:43:02 +0800

    full deploy

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", QuoteController.getAllQuotes);
 
