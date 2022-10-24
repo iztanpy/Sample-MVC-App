0bd5cbd72b377a5538e364a33f79d01997c2b3c8
iztanpy <61683649+iztanpy@users.noreply.github.com>
2022-10-24 20:41:38 +0800

    full deployment

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.get("/", QuoteController.getAllQuotes);
+app.get("/", QuoteController.getAllQuotes);
 
-// app.post("/post", QuoteController.addQuote);
+app.post("/post", QuoteController.addQuote);
 
-// app.put("/put", QuoteController.addQuote);
+app.put("/put", QuoteController.addQuote);
 
-// app.delete("/delete", QuoteController.deleteLastQuote);
+app.delete("/delete", QuoteController.deleteLastQuote);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
-import chai from "chai";
-import chaiHttp from "chai-http";
-import app from "../app.js";
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../app.js");
 // Configure chai
 chai.use(chaiHttp);
 chai.should();
