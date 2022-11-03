d3f7be7de0549cf46e4b86294c3d78ecbd73aa34
iztanpy <61683649+iztanpy@users.noreply.github.com>
2022-11-03 19:48:14 +0800

    Edit Dummy Quotes

--- dummy/quotes.js
+++ dummy/quotes.js
@@ -1,6 +1,15 @@
 const quotes = [
-  { id: 1, quote: "me", author: "wo" },
-  { id: 2, quote: "Hi", author: "we" },
+  {
+    id: 1,
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall",
+    author: "Nelson Mandela",
+  },
+  {
+    id: 2,
+    quote: "The way to get started is to quit talking and begin doing",
+    author: "Walt Disney",
+  },
 ];
 
 module.exports = quotes;
